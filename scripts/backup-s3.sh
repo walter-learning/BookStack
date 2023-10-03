465000fd4c6242f5e0395e730ad806445747f36a
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2023-10-03 14:54:25 +0200

    Add backup script

---
+++ scripts/backup-s3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f ../.env ]
+then
+  export $(cat ../.env | xargs)
+fi
+
+# S3 bucket details
+BUCKET_NAME="walter-academy"
+BACKUP_NAME="$(date +%Y-%m-%d-%H-%M-%S).sql.gz"
+
+# Backup the MySQL database
+mysqldump --user=${USER} --password=${PASSWORD} ${DATABASE} | gzip > /tmp/${BACKUP_NAME}
+
+# Upload the backup to S3
+aws s3 cp /tmp/${BACKUP_NAME} s3://${BUCKET_NAME}/backups/${BACKUP_NAME}
+
+# Remove the backup from the local machine
+rm /tmp/${BACKUP_NAME}
