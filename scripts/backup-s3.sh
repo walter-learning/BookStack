67261e3ef0f80452d49b56fef6d9c267be23d082
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2023-10-03 15:16:56 +0200

    Update backup script

--- scripts/backup-s3.sh
+++ scripts/backup-s3.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-if [ ! -f ../.env ]
-then
-  export $(cat ../.env | xargs)
-fi
-
 # S3 bucket details
 BUCKET_NAME="walter-academy"
 BACKUP_NAME="$(date +%Y-%m-%d-%H-%M-%S).sql.gz"
 
 # Backup the MySQL database
-mysqldump --user=${DB_USERNAME} --password=${DB_PASSWORD} ${DB_DATABASE} | gzip > /tmp/${BACKUP_NAME}
+sudo mysqldump bookstack | gzip > /tmp/${BACKUP_NAME}
 
 # Upload the backup to S3
 aws s3 cp /tmp/${BACKUP_NAME} s3://${BUCKET_NAME}/backups/${BACKUP_NAME}
