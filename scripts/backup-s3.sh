75cf488f2497ffac67d9b7581fafa91d87f2f24c
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2023-10-03 14:56:31 +0200

    Update variable

--- scripts/backup-s3.sh
+++ scripts/backup-s3.sh
@@ -10,7 +10,7 @@ BUCKET_NAME="walter-academy"
 BACKUP_NAME="$(date +%Y-%m-%d-%H-%M-%S).sql.gz"
 
 # Backup the MySQL database
-mysqldump --user=${USER} --password=${PASSWORD} ${DATABASE} | gzip > /tmp/${BACKUP_NAME}
+mysqldump --user=${DB_USERNAME} --password=${DB_PASSWORD} ${DB_DATABASE} | gzip > /tmp/${BACKUP_NAME}
 
 # Upload the backup to S3
 aws s3 cp /tmp/${BACKUP_NAME} s3://${BUCKET_NAME}/backups/${BACKUP_NAME}
