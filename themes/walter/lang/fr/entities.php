09baaf4471fc02b590212e5c30c791876c3f1f75
Your Name <you@example.com>
2023-10-16 14:52:21 +0000

    Change comment to feedback

--- themes/walter/lang/fr/entities.php
+++ themes/walter/lang/fr/entities.php
@@ -5,4 +5,22 @@
  */
 return [
     'pages_save' => 'Publier la page',
+
+    // Comments
+    'comment' => 'Feedback',
+    'comments' => 'Feedbacks',
+    'comment_add' => 'Ajouter un feedback',
+    'comment_placeholder' => 'Indiquez votre feedback ici',
+    'comment_count' => '{0} Pas de feedback|{1} Un feedback|[2,*] :count feedbacks',
+    'comment_save' => 'Envoyer le feedback',
+    'comment_new' => 'Nouveau feedback',
+    'comment_created' => 'envoyé :createDiff',
+    'comment_updated' => 'Mis à jour :updateDiff par :username',
+    'comment_updated_indicator' => 'Mis à jour',
+    'comment_deleted_success' => 'Feedback supprimé',
+    'comment_created_success' => 'Feedback ajouté',
+    'comment_updated_success' => 'Feedback mis à jour',
+    'comment_delete_confirm' => 'Êtes-vous sûr de vouloir supprimer ce feedback?',
+    'comment_in_reply_to' => 'En réponse à :commentId',
+    'comment_editor_explain' => 'Voici les feedbacks qui ont été laissés sur cette page. Les feedbacks peuvent être ajoutés et gérés en visualisant la page enregistrée.',
 ];
